b072eb621aa78a15bad18056b1c556ef05b7e7a1
William Lêdo <williamledoc@gmail.com>
2022-02-04 10:50:38 -0300

    mutation create author complete

---
+++ app/graphql/mutations/create_author.rb
@@ -0,0 +1,23 @@
+class Mutations::CreateAuthor < Mutations::BaseMutation
+    description "Creates Author"
+
+    argument :name, String, required: true
+    argument :email, String, required: true
+
+    field :author, Types::AuthorType, null: false
+    field :errors, [String], null: false
+
+    def resolve (name:, email:)
+
+        author = Author.new(name: name, email: email)
+
+
+        if(author.save)
+            {author: author, errors: []}
+        else
+            {author: nil, errrors: author.errors.full_messages}
+        end
+
+    end
+       
+end
